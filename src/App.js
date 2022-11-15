4154b29150403809e36a8abe659dbb5914b0c0c2
subhan <subhan.rehman@prolitus.com>
2022-11-15 13:06:51 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import CalendarComp from "./components/calendar/CalendarComp";
+import Media from "./components/media/Media";
+function App() {
+  const [date, setDate] = useState(new Date());
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+  }, [date]);
+  useEffect(() => {
+    console.log(date);
+    axios(
+      `/apod/planetary/v1/${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()}`
+    )
+      .then((res) => {
+        if (res) {
+          setLoading(false);
+        }
+        console.log(res.data.planetaryData);
+        setData(res.data.planetaryData);
+      })
+      .catch((err) => console.log(err));
+  }, [date]);
+
+  return (
+    <>
+      <div>
+        <div className="top-comp">
+          {loading ? (
+            <></>
+          ) : (
+            <div className="apod-title">{`Title : ${data?.title}`}</div>
+          )}
+          <div className="cal-box">
+            <CalendarComp
+              date={date}
+              setDate={setDate}
+              setLoading={setLoading}
+            />
+          </div>
+        </div>
+        {loading ? (
+          <div className="load">Loading...</div>
+        ) : (
+          <div>
+            <Media data={data} />
+          </div>
+        )}
+        {!loading && <div className="apod-explain">{data?.explanation}</div>}
+      </div>
+      )
+    </>
+  );
+}
+
+export default App;
